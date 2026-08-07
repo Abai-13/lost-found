72432d4bcf6e36fbe8c8a71f94234d2d33cda396
Baicx <1484356094@qq.com>
2026-08-07 10:42:09 +0800

    init: gitignore

--- src/main/java/com/lostfound/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.lostfound.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.lostfound.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
